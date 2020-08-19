224a02639a60079d51d244dfa5e72ff265b6bd14
Beast51 <7007500@gmail.com>
2020-08-19 19:22:26 +0300

    Add spinner(loading) to fetchedPosts

--- src/components/FetchedPosts.js
+++ src/components/FetchedPosts.js
@@ -2,10 +2,16 @@ import React from "react";
 import Post from "./Post";
 import {useDispatch, useSelector} from "react-redux";
 import {fetchPosts} from "../redux/action";
+import {Loader} from "./Loader";
 
 export default () => {
     const dispatch = useDispatch();
-    const posts = useSelector((state) => state.posts.fetchedPosts);
+    const posts = useSelector(state => state.posts.fetchedPosts);
+    const loading = useSelector(state => state.app.loading);
+
+    if (loading) {
+        return <Loader />
+    }
 
 
     if (!posts.length) {
